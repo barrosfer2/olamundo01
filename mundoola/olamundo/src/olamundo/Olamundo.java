0f92bab106a56da2e888d9fb9513f508c28076ec
barrosfer2 <49165654+barrosfer2@users.noreply.github.com>
2019-05-02 21:19:31 -0300

    repositorio012

---
+++ mundoola/olamundo/src/olamundo/Olamundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundo;
+
+/**
+ *
+ * @author GC-Aula
+ */
+public class Olamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Ola Mundo");
+       System.out.println("Aula GC 02/05/2019");
+    }
+    
+}
---
+++ mundoola/olamundo/src/olamundo/dasd.java
@@ -0,0 +1,11 @@
+
+package olamundo;
+
+public class dasd {
+   
+public static void main(String[] args) {
+    System.out.println( "Aula GC 02/05/2019");
+            
+    }
+    
+}
